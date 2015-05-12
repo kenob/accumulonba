7b48abf8cca77ee76a837f24c738235986cf0994
Oghenekeno <keno.ob@live.com>
2015-05-12 13:05:24 -0400

    Added full name

--- scripts/run_latest.sh
+++ scripts/run_latest.sh
@@ -1,3 +1,28 @@
-./deploy_latest.sh
 
-~/cdse/accumulo/accumulo-1.4.2/bin/tool.sh Wc.jar -libjars /home/guest/cdse/accumulo/accumulo-1.4.2/lib/* Main -zz acc,guestvb -i /user/guest/teamfeed-1430859104/ -t wordCount
+cd ~/accumulonba
+
+git pull
+
+cd ~/hw5
+
+echo "Updating source file..."
+cp -r /home/guest/accumulonba/src/edu \
+/home/guest/hw5/src/main/java/
+
+mvn compile
+
+echo "Generating updated jar..."
+mvn package
+
+echo "Copying latest team feed to HDFS..."
+
+hadoop fs -rmr /user/guest/teamfeed
+
+hadoop fs -copyFromLocal ~/accumulonba/scripts/teamfeed* /user/guest/teamfeed
+
+~/cdse/accumulo/accumulo-1.4.2/bin/tool.sh \
+/home/guest/hw5/target/hw5-1.0-SNAPSHOT.jar \
+edu.cse.buffalo.cse587.Main acc guestvb \
+/user/guest/teamfeed/east,/user/guest/teamfeed/west nbaTeams
+
+
